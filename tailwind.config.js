c80fdc209b08310667e9c14774c527f4dcf69721
shohail <shohail@cloudthing.com>
2020-08-10 20:47:38 +0530

    init css

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#fa1616"
+      },
+      fontFamily: {
+        body: ['Nunito']
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
